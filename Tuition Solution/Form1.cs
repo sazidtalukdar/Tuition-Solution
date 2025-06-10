56ee41ef9f34286ccb75784cb0577d586312d7cb
Sajid Mohammad Talukdar <23-53063-3@student.aiub.edu>
2025-06-10 23:16:33 +0600

    add  comment class

--- Tuition Solution/Form1.cs
+++ Tuition Solution/Form1.cs
@@ -9,7 +9,7 @@ namespace Tuition_Solution
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string s = "sazid";
+            string s = "sazid";//class
         }
     }
 }
