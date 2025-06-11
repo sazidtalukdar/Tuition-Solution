6b42f41c8978a970c660f728d8a6144d7395dd3c
Sajid Mohammad Talukdar <23-53063-3@student.aiub.edu>
2025-06-12 00:06:02 +0600

    add api

---
+++ Tuition Solution/otp_and_code.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Tuition_Solution
+{
+    internal class otp_and_code
+    {
+
+        public static int get_code()
+        {
+            var random = new Random();
+            int code = random.Next(100000, 999999); 
+            return code;
+        }
+
+        public static void SendSms(string number,string sms)
+        {
+            string apiKey = "jMlv1LIPfmMf69ATOmB5";
+            string senderId = "8809617619638";
+
+            string url = $"http://bulksmsbd.net/api/smsapi?api_key={apiKey}&type=text&number={number}&senderid={senderId}&message={sms}";
+
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadString(url);
+                //string response = client.DownloadString(url);
+                MessageBox.Show("Otp is send to your number");
+            }
+        }
+
+
+
+        public static bool verify_code( int user_code , string phone_no)
+        {
+
+            int code = get_code();
+
+            SendSms(phone_no, $"your otp is {code} Do not share your opt with onay one . Thank you");
+
+            if (code == user_code)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
