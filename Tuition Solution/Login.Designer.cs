06bf2776a2455b5bdb4a30dca5bb3ce723fee76a
argho <argho@LAPTOP-H80BFKML>
2025-06-13 00:57:14 +0600

    Revert "add"
    This reverts commit 29e951e4a7dde52bfd26d44c1af4c41a5611ab8e.

--- Tuition Solution/Login.Designer.cs
+++ Tuition Solution/Login.Designer.cs
@@ -31,138 +31,82 @@
             label1 = new Label();
             label2 = new Label();
             userbox = new TextBox();
-            button1 = new Button();
-            linkLabel1 = new LinkLabel();
-            linkLabel2 = new LinkLabel();
-            label3 = new Label();
-            pictureBox1 = new PictureBox();
             passbox = new TextBox();
-            checkBox1 = new CheckBox();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
             // 
             // label1
             // 
-            label1.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.ForeColor = Color.FromArgb(0, 0, 192);
-            label1.Location = new Point(37, 183);
+            label1.AutoSize = true;
+            label1.Location = new Point(87, 83);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(170, 42);
+            label1.Size = new Size(38, 15);
             label1.TabIndex = 0;
-            label1.Text = "Email";
-            label1.Click += label1_Click;
+            label1.Text = "label1";
             // 
             // label2
             // 
-            label2.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.ForeColor = Color.FromArgb(0, 0, 192);
-            label2.Location = new Point(37, 275);
+            label2.AutoSize = true;
+            label2.Location = new Point(87, 128);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(170, 42);
+            label2.Size = new Size(38, 15);
             label2.TabIndex = 1;
-            label2.Text = "Password";
+            label2.Text = "label2";
             // 
             // userbox
             // 
-            userbox.Location = new Point(250, 183);
-            userbox.Multiline = true;
+            userbox.Location = new Point(298, 68);
+            userbox.Margin = new Padding(2, 2, 2, 2);
             userbox.Name = "userbox";
-            userbox.Size = new Size(198, 42);
+            userbox.Size = new Size(140, 23);
             userbox.TabIndex = 2;
-            // 
-            // button1
-            // 
-            button1.BackColor = Color.FromArgb(255, 128, 128);
-            button1.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.Location = new Point(137, 464);
-            button1.Name = "button1";
-            button1.Size = new Size(223, 50);
-            button1.TabIndex = 4;
-            button1.Text = "Login";
-            button1.UseVisualStyleBackColor = false;
-            // 
-            // linkLabel1
-            // 
-            linkLabel1.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            linkLabel1.LinkColor = Color.Red;
-            linkLabel1.Location = new Point(37, 375);
-            linkLabel1.Name = "linkLabel1";
-            linkLabel1.Size = new Size(211, 36);
-            linkLabel1.TabIndex = 5;
-            linkLabel1.TabStop = true;
-            linkLabel1.Text = "Forgot Password ?";
-            // 
-            // linkLabel2
-            // 
-            linkLabel2.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            linkLabel2.LinkColor = Color.Green;
-            linkLabel2.Location = new Point(96, 556);
-            linkLabel2.Name = "linkLabel2";
-            linkLabel2.Size = new Size(343, 50);
-            linkLabel2.TabIndex = 6;
-            linkLabel2.TabStop = true;
-            linkLabel2.Text = "Create a New Account ?";
-            // 
-            // label3
-            // 
-            label3.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.ForeColor = Color.Maroon;
-            label3.Location = new Point(64, 67);
-            label3.Name = "label3";
-            label3.Size = new Size(375, 45);
-            label3.TabIndex = 7;
-            label3.Text = "Welcome Back :)";
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Image = Properties.Resources.ChatGPT_Image_Jun_12__2025__01_49_43_AM;
-            pictureBox1.Location = new Point(543, 0);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(472, 645);
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox1.TabIndex = 8;
-            pictureBox1.TabStop = false;
             // 
             // passbox
             // 
-            passbox.Location = new Point(250, 272);
-            passbox.Multiline = true;
+            passbox.Location = new Point(298, 128);
+            passbox.Margin = new Padding(2, 2, 2, 2);
             passbox.Name = "passbox";
-            passbox.Size = new Size(198, 45);
+            passbox.Size = new Size(140, 23);
             passbox.TabIndex = 3;
             // 
-            // checkBox1
+            // button1
             // 
-            checkBox1.Location = new Point(312, 374);
-            checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(162, 36);
-            checkBox1.TabIndex = 9;
-            checkBox1.Text = "Show Password";
-            checkBox1.UseVisualStyleBackColor = true;
+            button1.Location = new Point(122, 251);
+            button1.Margin = new Padding(2, 2, 2, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(134, 34);
+            button1.TabIndex = 4;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(305, 176);
+            button2.Margin = new Padding(2, 2, 2, 2);
+            button2.Name = "button2";
+            button2.Size = new Size(170, 109);
+            button2.TabIndex = 5;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += login_bt_click;
             // 
             // Login
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.ControlLight;
-            ClientSize = new Size(1018, 644);
-            Controls.Add(checkBox1);
-            Controls.Add(passbox);
-            Controls.Add(pictureBox1);
-            Controls.Add(label3);
-            Controls.Add(linkLabel2);
-            Controls.Add(linkLabel1);
+            ClientSize = new Size(732, 495);
+            Controls.Add(button2);
             Controls.Add(button1);
+            Controls.Add(passbox);
             Controls.Add(userbox);
             Controls.Add(label2);
             Controls.Add(label1);
-            FormBorderStyle = FormBorderStyle.None;
-            Margin = new Padding(4, 5, 4, 5);
             Name = "Login";
-            StartPosition = FormStartPosition.CenterScreen;
             Text = "Login";
             Load += Form1_Load;
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -172,12 +116,8 @@
         private Label label1;
         private Label label2;
         private TextBox userbox;
-        private Button button1;
-        private LinkLabel linkLabel1;
-        private LinkLabel linkLabel2;
-        private Label label3;
-        private PictureBox pictureBox1;
         private TextBox passbox;
-        private CheckBox checkBox1;
+        private Button button1;
+        private Button button2;
     }
 }
