d26882ec4ef112cc599c02abf3360dbdafb26c4f
Kazi-Samia-Mostofa <kazisamiamostofa1001@gmail.com>
2025-06-13 05:19:21 +0600

    Reg Full

--- Tuition Solution/RegistrationForm.Designer.cs
+++ Tuition Solution/RegistrationForm.Designer.cs
@@ -28,19 +28,359 @@
         /// </summary>
         private void InitializeComponent()
         {
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            linkLabel1 = new LinkLabel();
+            label8 = new Label();
+            button1 = new Button();
+            checkBox2 = new CheckBox();
+            textBox4 = new TextBox();
+            label7 = new Label();
+            checkBox1 = new CheckBox();
+            textBox3 = new TextBox();
+            label6 = new Label();
+            radioButton4 = new RadioButton();
+            radioButton3 = new RadioButton();
+            label5 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            label2 = new Label();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
             SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.WhatsApp_Image_2025_06_12_at_17_05_04_3686d490;
+            pictureBox1.Location = new Point(-4, -2);
+            pictureBox1.Margin = new Padding(2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(332, 577);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(128, 255, 255, 255);
+            panel1.Controls.Add(linkLabel1);
+            panel1.Controls.Add(label8);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(checkBox2);
+            panel1.Controls.Add(textBox4);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(checkBox1);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(radioButton4);
+            panel1.Controls.Add(radioButton3);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(radioButton2);
+            panel1.Controls.Add(radioButton1);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Font = new Font("Segoe UI", 7.8F);
+            panel1.Location = new Point(375, 22);
+            panel1.Margin = new Padding(2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(338, 529);
+            panel1.TabIndex = 1;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.BackColor = Color.Transparent;
+            linkLabel1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            linkLabel1.LinkColor = Color.FromArgb(28, 127, 168);
+            linkLabel1.Location = new Point(216, 482);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(58, 15);
+            linkLabel1.TabIndex = 19;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Click here";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.Transparent;
+            label8.Font = new Font("Segoe UI", 10.2F);
+            label8.Location = new Point(55, 479);
+            label8.Name = "label8";
+            label8.Size = new Size(165, 19);
+            label8.TabIndex = 18;
+            label8.Text = "Already have an account?";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(28, 127, 168);
+            button1.Font = new Font("Segoe UI", 10.2F);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(48, 447);
+            button1.Name = "button1";
+            button1.Size = new Size(237, 29);
+            button1.TabIndex = 17;
+            button1.Text = "Sign Up";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.BackColor = Color.Transparent;
+            checkBox2.Location = new Point(176, 424);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(107, 17);
+            checkBox2.TabIndex = 16;
+            checkBox2.Text = "Show Password";
+            checkBox2.UseVisualStyleBackColor = false;
+            checkBox2.CheckedChanged += checkBox2_CheckedChanged;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold);
+            textBox4.Location = new Point(48, 392);
+            textBox4.Margin = new Padding(2);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.PasswordChar = '*';
+            textBox4.Size = new Size(233, 27);
+            textBox4.TabIndex = 15;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold);
+            label7.Location = new Point(41, 370);
+            label7.Margin = new Padding(2, 0, 2, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(140, 20);
+            label7.TabIndex = 14;
+            label7.Text = "Confirm Password :";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.BackColor = Color.Transparent;
+            checkBox1.Location = new Point(176, 350);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(107, 17);
+            checkBox1.TabIndex = 13;
+            checkBox1.Text = "Show Password";
+            checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold);
+            textBox3.Location = new Point(48, 318);
+            textBox3.Margin = new Padding(2);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.PasswordChar = '*';
+            textBox3.Size = new Size(233, 27);
+            textBox3.TabIndex = 12;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold);
+            label6.Location = new Point(41, 296);
+            label6.Margin = new Padding(2, 0, 2, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(81, 20);
+            label6.TabIndex = 11;
+            label6.Text = "Password :";
+            label6.Click += label6_Click;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.BackColor = Color.Transparent;
+            radioButton4.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold);
+            radioButton4.Location = new Point(176, 270);
+            radioButton4.Margin = new Padding(2);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(76, 24);
+            radioButton4.TabIndex = 10;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "Female";
+            radioButton4.UseVisualStyleBackColor = false;
+            radioButton4.CheckedChanged += radioButton4_CheckedChanged;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.BackColor = Color.Transparent;
+            radioButton3.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold);
+            radioButton3.Location = new Point(76, 270);
+            radioButton3.Margin = new Padding(2);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(61, 24);
+            radioButton3.TabIndex = 9;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Male";
+            radioButton3.UseVisualStyleBackColor = false;
+            radioButton3.CheckedChanged += radioButton3_CheckedChanged;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold);
+            label5.Location = new Point(41, 248);
+            label5.Margin = new Padding(2, 0, 2, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(67, 20);
+            label5.TabIndex = 8;
+            label5.Text = "Gender :";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold);
+            textBox2.Location = new Point(48, 210);
+            textBox2.Margin = new Padding(2);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(233, 27);
+            textBox2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold);
+            label4.Location = new Point(41, 188);
+            label4.Margin = new Padding(2, 0, 2, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(122, 20);
+            label4.TabIndex = 6;
+            label4.Text = "Phone Number :";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold);
+            textBox1.Location = new Point(48, 148);
+            textBox1.Margin = new Padding(2);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(233, 27);
+            textBox1.TabIndex = 5;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold);
+            label3.Location = new Point(41, 126);
+            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(86, 20);
+            label3.TabIndex = 4;
+            label3.Text = "Username :";
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.BackColor = Color.Transparent;
+            radioButton2.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold);
+            radioButton2.Location = new Point(176, 98);
+            radioButton2.Margin = new Padding(2);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(80, 24);
+            radioButton2.TabIndex = 3;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Student";
+            radioButton2.UseVisualStyleBackColor = false;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.BackColor = Color.Transparent;
+            radioButton1.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold);
+            radioButton1.Location = new Point(76, 100);
+            radioButton1.Margin = new Padding(2);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(63, 24);
+            radioButton1.TabIndex = 2;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Tutor";
+            radioButton1.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(41, 74);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(116, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Registering AS :";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(2, 136, 209);
+            label1.Location = new Point(104, 32);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(120, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Registration";
             // 
             // RegistrationForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.Gainsboro;
+            ClientSize = new Size(758, 575);
+            Controls.Add(panel1);
+            Controls.Add(pictureBox1);
             Name = "RegistrationForm";
             Text = "RegistrationForm";
             Load += RegistrationForm_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private Label label1;
+        private Label label2;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox2;
+        private Label label5;
+        private RadioButton radioButton4;
+        private RadioButton radioButton3;
+        private Label label6;
+        private TextBox textBox3;
+        private CheckBox checkBox1;
+        private TextBox textBox4;
+        private Label label7;
+        private CheckBox checkBox2;
+        private Button button1;
+        private LinkLabel linkLabel1;
+        private Label label8;
     }
 }
--- Tuition Solution/RegistrationForm.cs
+++ Tuition Solution/RegistrationForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Tuition_Solution
 {
@@ -32,7 +33,65 @@ namespace Tuition_Solution
 
         }
 
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
 
+        }
 
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                textBox3.UseSystemPasswordChar = true;
+            }
+            else
+            {
+                textBox3.UseSystemPasswordChar = false;
+            }
+        }
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+            {
+                textBox4.UseSystemPasswordChar = true;
+            }
+            else
+            {
+                textBox4.UseSystemPasswordChar = false;
+            }
+        }
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.White;
+            button1.ForeColor = Color.FromArgb(28, 127, 168);
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.FromArgb(28, 127, 168);
+            button1.ForeColor = Color.White;
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        /*private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }*/
     }
 }
